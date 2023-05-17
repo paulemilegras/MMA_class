9c11d39ff487302adb5f27be17006a49a85c623c
Paul-Emile Gras <60232231+paulemilegras@users.noreply.github.com>
2023-05-17 19:53:05 +0200

    Update after_copy_paste.py add clustering

--- after_copy_paste.py
+++ after_copy_paste.py
@@ -1,5 +1,8 @@
 ### Here we have the code to copy paste
 
+
+# III. Tree-based methods
+
 # III.b.1 : Build subsets
 
 ## --> Copy-paste the code snippet in the above cell (data preprocessing, train and test data)
@@ -28,7 +31,6 @@ for i in skuSet[1:]: #Iteration over items
 
     
 # III.2.
-
 
 # b) Run model: --> copy-paste from III.1.a.
 
@@ -106,6 +108,74 @@ print("Time to compute:",round(t,3)," sec")
 
 
 
+# IV. Clustering methods
+
+# IV.1. --> nothing to add
+
+
+# IV.2.
+
+## IV.2.a. Loop to find the optimal number of clusters
+
+
+#Clustering --> copy-paste from IV.1.a.
+
+d = len(colnames) #d is the number of columns
+X_clus = np.zeros((len(skuSet), d))
+
+for sku in skuSet:
+    sku_index=sku-1
+    X_clus[sku_index, :] = np.mean(X_dict[sku]['train'], axis = 0)
+X_clus
+
+kmeans = KMeans(n_clusters=z, random_state=0).fit(X_clus)
+
+
+#Build dataset and run model --> copy-paste from IV.1.c. (Loop with all clusters)
+
+#Loop
+y_clus_pred = []
+y_clus_test = []
+
+for j in range(z):
+
+  ## 1. Get indices of items in cluster j 
+  clus_items = list(np.where(kmeans.labels_ == j)[0])
+
+  ## 2. Build dataset
+
+  ##Initialization 
+  #X
+  X_clus_j_train = X_dict[skuSet[clus_items[0]]]['train'] #initialization with first item of the cluster
+  X_clus_j_test = X_dict[skuSet[clus_items[0]]]['test']
+  #y
+  y_clus_j_train = list(y_dict[skuSet[clus_items[0]]]['train']) #initialization with first item of the cluster
+  y_clus_j_test = list(y_dict[skuSet[clus_items[0]]]['test'])
+  ##Loop 
+  for idx in clus_items[1:]: #Iteration over items
+    sku=skuSet[idx]
+    #X
+    X_clus_j_train = np.concatenate((X_clus_j_train, X_dict[sku]['train']), axis = 0) #Bringing together the training set for the cluster
+    X_clus_j_test = np.concatenate((X_clus_j_test, X_dict[sku]['test']), axis = 0)
+    #y
+    y_clus_j_train += list(y_dict[sku]['train'])
+    y_clus_j_test += list(y_dict[sku]['test'])
+
+  ## 3. Run model
+  model_clus_j = LinearRegression().fit(X_clus_j_train, y_clus_j_train)
+  y_clus_pred += list(model_clus_j.predict(X_clus_j_test))
+  y_clus_test += y_clus_j_test
+
+
+#Comparison of results --> write manually
+
+
+
+
+
+
+
+## At the very end of the loop (no indentation) --> write manually
 
 
 
@@ -114,6 +184,58 @@ print("Time to compute:",round(t,3)," sec")
 
 
 
+
+## IV.2.a. Loop to find the optimal number of clusters
+
+# Clustering --> copy-paste from IV.1.a.
+
+d = len(colnames) #d is the number of columns
+X_clus = np.zeros((len(skuSet), d))
+
+for sku in skuSet:
+    sku_index=sku-1
+    X_clus[sku_index, :] = np.mean(X_dict[sku]['train'], axis = 0)
+X_clus
+
+kmeans = KMeans(n_clusters=z, random_state=0).fit(X_clus)
+
+# Build dataset and run model for each cluster --> copy-paste from IV.1.c. (Loop with all clusters)
+
+#Loop
+y_clus_pred = []
+y_clus_test = []
+
+for j in range(z):
+
+  ## 1. Get indices of items in cluster j 
+  clus_items = list(np.where(kmeans.labels_ == j)[0])
+
+  ## 2. Build dataset
+
+  ##Initialization 
+  #X
+  X_clus_j_train = X_dict[skuSet[clus_items[0]]]['train'] #initialization with first item of the cluster
+  X_clus_j_test = X_dict[skuSet[clus_items[0]]]['test']
+  #y
+  y_clus_j_train = list(y_dict[skuSet[clus_items[0]]]['train']) #initialization with first item of the cluster
+  y_clus_j_test = list(y_dict[skuSet[clus_items[0]]]['test'])
+  ##Loop 
+  for idx in clus_items[1:]: #Iteration over items
+    sku=skuSet[idx]
+    #X
+    X_clus_j_train = np.concatenate((X_clus_j_train, X_dict[sku]['train']), axis = 0) #Bringing together the training set for the cluster
+    X_clus_j_test = np.concatenate((X_clus_j_test, X_dict[sku]['test']), axis = 0)
+    #y
+    y_clus_j_train += list(y_dict[sku]['train'])
+    y_clus_j_test += list(y_dict[sku]['test'])
+
+  ## 3. Run model
+  model_clus_j = LinearRegression().fit(X_clus_j_train, y_clus_j_train)
+  y_clus_pred += list(model_clus_j.predict(X_clus_j_test))
+  y_clus_test += y_clus_j_test
+
+  
+#### Print Results #### --> write manually
 
 
 
